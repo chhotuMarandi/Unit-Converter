107242fc3e9737f04d50935f6bb6a692377bd330
chhotu marandi <cmarandi29@gmail.com>
2022-08-07 01:29:38 +0530

    make buttons in working condition

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+let input = document.getElementById('input');
 
+let buttons = document.getElementsByClassName('number');
+
+for (i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function () {
+    input.value = this.id;
+  });
+}
