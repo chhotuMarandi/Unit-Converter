4af59322b922e3bf7b40c3c0dc5ce0e51c304610
chhotu marandi <cmarandi29@gmail.com>
2022-08-07 16:41:58 +0530

    make clear and cut buttons functioning

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 let display = document.getElementById('display');
 
-// input.addEventListener('click', function () {
-//   console.log('click');
+// display.map((display) => {
+//   display.addEventListener('click', (e) => {
+//     console.log('one');
+//     console.log(e.target);
+//   });
 // });
 
 let buttons = Array.from(document.getElementsByClassName('buttons'));
 
 buttons.map((buttons) => {
   buttons.addEventListener('click', (e) => {
-    console.log(e.target.innerText);
+    // console.log(e.target.innerText);
 
     switch (e.target.innerText) {
+      case 'AC':
+        display.innerText = '';
+        break;
+      case '*':
+        if (display.innerText) {
+          display.innerText = display.innerText.slice(0, -1);
+        }
+        break;
       default:
         display.innerText += e.target.innerText;
     }
