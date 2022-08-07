783b24088fdf22ee80fb9153f9d328b8dcfd406d
chhotu marandi <cmarandi29@gmail.com>
2022-08-07 11:47:16 +0530

    display when button is pressed

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-let input = document.getElementById('input');
+let display = document.getElementById('display');
 
-let buttons = document.getElementsByClassName('number');
+// input.addEventListener('click', function () {
+//   console.log('click');
+// });
 
-for (i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', function () {
-    input.value = this.id;
+let buttons = Array.from(document.getElementsByClassName('buttons'));
+
+buttons.map((buttons) => {
+  buttons.addEventListener('click', (e) => {
+    console.log(e.target.innerText);
+
+    switch (e.target.innerText) {
+      default:
+        display.innerText += e.target.innerText;
+    }
   });
-}
+});
 
-const kuchbhi = () => {
-    let inputValue =
-}
+// for (i = 0; i < buttons.length; i++) {
+//   buttons[i].addEventListener('click', function () {
+//     input.value = this.id;
+//   });
+// }
