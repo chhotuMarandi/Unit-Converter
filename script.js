ed655f7566046080117d823b2a5e4023d4bf3753
chhotu marandi <cmarandi29@gmail.com>
2022-08-08 16:50:30 +0530

    select unit value check

--- script.js
+++ script.js
@@ -1,35 +1,25 @@
-let display = document.getElementById('display');
+function unitValue() {
+  var selectValue = document.getElementById('unit_1').value;
+  console.log(selectValue);
+}
 
-// display.map((display) => {
-//   display.addEventListener('click', (e) => {
-//     console.log('one');
-//     console.log(e.target);
+// let buttons = Array.from(document.getElementsByClassName('buttons'));
+
+// buttons.map((buttons) => {
+//   buttons.addEventListener('click', (e) => {
+//     // console.log(e.target.innerText);
+
+//     switch (e.target.innerText) {
+//       case 'AC':
+//         display.innerText = '';
+//         break;
+//       case '*':
+//         if (display.innerText) {
+//           display.innerText = display.innerText.slice(0, -1);
+//         }
+//         break;
+//       default:
+//         display.innerText += e.target.innerText;
+//     }
 //   });
 // });
-
-let buttons = Array.from(document.getElementsByClassName('buttons'));
-
-buttons.map((buttons) => {
-  buttons.addEventListener('click', (e) => {
-    // console.log(e.target.innerText);
-
-    switch (e.target.innerText) {
-      case 'AC':
-        display.innerText = '';
-        break;
-      case '*':
-        if (display.innerText) {
-          display.innerText = display.innerText.slice(0, -1);
-        }
-        break;
-      default:
-        display.innerText += e.target.innerText;
-    }
-  });
-});
-
-// for (i = 0; i < buttons.length; i++) {
-//   buttons[i].addEventListener('click', function () {
-//     input.value = this.id;
-//   });
-// }
