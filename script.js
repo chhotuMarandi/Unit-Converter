6bca421e0710beb3a72135f062964a326fa88d36
chhotu marandi <cmarandi29@gmail.com>
2022-09-02 15:33:46 +0530

    set formula

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const converter = () => {
   const inputOne = document.getElementById('inputOne').value;
   // console.log(input);
-  const inputTwo = document.getElementById('inputTwo').value;
+  const inputTwo = (document.getElementById('inputTwo').value = inputOne);
   // console.log(result);
 
   const selectorOne = document.getElementById('unitOne');
-  const selectedValue = selectorOne.value;
-  console.log(selectedValue);
+  const selectedValueOne = selectorOne.value;
+  // console.log(selectedValueOne);
 
-  if (selectedValue == 'feet') {
-    inputTwo = feetToMeter(inputOne);
-  } else if (selectedValue == 'inch')
+  const selectorTwo = document.getElementById('unitTwo');
+  const selectedValueTwo = selectorTwo.value;
+  console.log(selectedValueTwo);
+
+  // const feetToMeter = (f) => {
+  //   let feet = Math.round(f * 0.304);
+  //   return feet;
+  // };
+
+  // if (selectedValueOne == 'feet' && selectedValueTwo == 'meter') {
+  //   inputTwo = feetToMeter(inputOne);
+  // } else if (selectedValueOne == 'inch' && selectedValueTwo == 'feet') {
+  //   inputTwo = inchToFeet(inputOne);
+  // }
 };
