eeba4d95e8cea540392731b1bf1887818f0e017d
chhotu marandi <cmarandi29@gmail.com>
2022-09-02 14:16:26 +0530

    select options

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-const inputOne = document.getElementById('inputOne');
+const converter = () => {
+  const inputOne = document.getElementById('inputOne').value;
+  // console.log(input);
+  const inputTwo = document.getElementById('inputTwo').value;
+  // console.log(result);
+
+  const selectorOne = document.getElementById('unitOne');
+  const selectedValue = selectorOne.value;
+  console.log(selectedValue);
+
+  if (selectedValue == 'feet') {
+    inputTwo = feetToMeter(inputOne);
+  } else if (selectedValue == 'inch')
+};
