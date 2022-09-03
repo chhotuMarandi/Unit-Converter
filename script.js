0b78e2887710daf8e8d78b5aedf58b8cd0a2857f
chhotu marandi <cmarandi29@gmail.com>
2022-09-03 12:17:05 +0530

    input field is working now

--- script.js
+++ script.js
@@ -4,16 +4,20 @@ let keypad = document.querySelector('#btn');
 let unitOne = document.getElementById('unitOne');
 let unitTwo = document.getElementById('unitTwo');
 //inputs
+
 let inputOne = document.getElementById('inputOne');
 let inputTwo = document.getElementById('inputTwo');
 
 let unitOneValue = unitOne.value;
 let unitTwoValue = unitTwo.value;
+let inputOneValue = inputOne.value;
+let inputTwoValue = inputTwo.value;
+//buttons...
 
 const btnValue = (e) => {
   if (e.target !== e.currentTarget) {
     let clickedItem = e.target.id;
-    console.log('hello' + clickedItem);
+    console.log(clickedItem);
   }
 };
 
@@ -21,25 +25,23 @@ const convertResult = () => {
   unitOneValue = unitOne.value;
   unitTwoValue = unitTwo.value;
 
-  //buttons
-
-  // let keypad = document.querySelector('#btn');
-
-  // keypad.addEventListener('click', btnValue);
+  inputOneValue = inputOne.value;
+  inputTwoValue = inputTwo.value;
+  console.log(inputOneValue);
 
   // const btnValue = (e) => {
   //   if (e.target !== e.currentTarget) {
   //     let clickedItem = e.target.id;
-  //     alert('hello' + clickedItem);
+  //     console.log(clickedItem);
   //   }
   // };
 
   //check conditions
 
   if (unitOneValue === 'inch' && unitTwoValue === 'feet') {
-    inputTwo.value = inputOne.value * 0.083;
+    inputTwo.value = (inputOne.value * 0.083).toFixed(2);
   } else if (unitOneValue === 'inch' && unitTwoValue === 'cm') {
-    inputTwo.value = inputOne.value * 2.54;
+    inputTwo.value = (inputOne.value * 2.54).toFixed(2);
   } else if (unitOneValue === 'inch' && unitTwoValue === 'inch') {
     inputTwo.value = inputOne.value;
   } else if (unitOneValue === 'feet' && unitTwoValue === 'feet') {
@@ -55,31 +57,11 @@ const convertResult = () => {
   } else if (unitOneValue === 'cm' && unitTwoValue === 'inch') {
     inputTwo.value = (inputOne.value / 2.54).toFixed(2);
   }
-
-  // if (unitOneValue === 'inch' && unitTwoValue === 'feet') {
-  //   inputOne.value = inputTwo.value * 0.083;
-  // } else if (unitOneValue === 'inch' && unitTwoValue === 'cm') {
-  //   inputOne.value = inputTwo.value * 2.54;
-  // } else if (unitOneValue === 'inch' && unitTwoValue === 'inch') {
-  //   inputOne.value = inputTwo.value;
-  // } else if (unitOneValue === 'feet' && unitTwoValue === 'feet') {
-  //   inputOne.value = inputTwo.value;
-  // } else if (unitOneValue === 'feet' && unitTwoValue === 'cm') {
-  //   inputOne.value = (inputTwo.value * 30.48).toFixed(2);
-  // } else if (unitOneValue === 'feet' && unitTwoValue === 'inch') {
-  //   inputOne.value = (inputTwo.value * 12).toFixed(2);
-  // } else if (unitOneValue === 'cm' && unitTwoValue === 'feet') {
-  //   inputOne.value = (inputTwo.value / 30.48).toFixed(2);
-  // } else if (unitOneValue === 'cm' && unitTwoValue === 'cm') {
-  //   inputOne.value = inputTwo.value;
-  // } else if (unitOneValue === 'cm' && unitTwoValue === 'inch') {
-  //   inputOne.value = (inputTwo.value / 2.54).toFixed(2);
-  // }
 };
 
 //Event Listener
 
-inputOne.addEventListener('keyup', convertResult);
+inputOne.addEventListener('input', convertResult);
 unitOne.addEventListener('change', convertResult);
 unitTwo.addEventListener('change', convertResult);
 keypad.addEventListener('click', btnValue);
